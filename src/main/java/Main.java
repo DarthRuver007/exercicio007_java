164855b6da0fc9ba55c1ecc700f6aee6735d3ea8
douglas <douglas@10.146.192.84>
2026-04-19 14:53:00 -0300

    init: Início do projeto.

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String nome = JOptionPane.showInputDialog("Qual seu nome?");
+
+        JOptionPane.showMessageDialog(null, "Bem-vindo!\n" + nome);
+    }
+}
